984e566908520acb61b33c544eb7896fd68b8b80
ewenbaron <ewen.baron@ericsson.com>
2019-06-03 11:19:27 +0200

    Restore S1AP test

---
+++ mts-asn1-per/src/test/java/com/ericsson/mts/asn1/S1APTests.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 Ericsson, https://www.ericsson.com/en
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.ericsson.mts.asn1;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class S1APTests extends AbstractTests {
+
+    @BeforeAll
+    static public void init() {
+        try {
+            asn1Translator = new ASN1Translator(new PERFactory(true), Arrays.asList(S1APTests.class.getResourceAsStream("/grammar/S1AP/S1AP.asn")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testS1AP() throws Exception {
+        test("S1AP-PDU", "/data/S1AP/S1AP-1.bin", "/data/S1AP/S1AP-1.json", "/data/S1AP/S1AP-1.xml");
+    }
+}
